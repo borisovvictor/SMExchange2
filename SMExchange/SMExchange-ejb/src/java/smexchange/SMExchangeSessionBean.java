e645ca6d8b18d2568f39aa48ff71ab5c5d9d2c43
v1cb <victor.borisov1@gmail.com>
2016-11-30 17:05:35 +0300

    Some changes

---
+++ SMExchange/SMExchange-ejb/src/java/smexchange/SMExchangeSessionBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smexchange;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Victor
+ */
+@Stateless
+public class SMExchangeSessionBean implements SMExchangeSessionBeanRemote, SMExchangeSessionBeanLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
